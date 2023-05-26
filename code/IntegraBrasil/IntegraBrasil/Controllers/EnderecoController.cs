8fe932ff30d7f6032ea9f456b86eed171cb6c8f9
joao victor <joaovictormdasilva676@gmail.com>
2023-05-26 20:56:05 -0300

    Created two controllers

---
+++ code/IntegraBrasil/IntegraBrasil/Controllers/EnderecoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntegraBrasil.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class EnderecoController : ControllerBase
+    {
+    }
+}
