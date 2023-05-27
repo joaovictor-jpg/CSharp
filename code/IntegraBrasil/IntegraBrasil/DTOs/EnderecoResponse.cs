22c42153ca2bff17269e2c73dc417d1603415dab
joao victor <joaovictormdasilva676@gmail.com>
2023-05-26 21:44:14 -0300

    Created DTOs

---
+++ code/IntegraBrasil/IntegraBrasil/DTOs/EnderecoResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IntegraBrasil.DTOs
+{
+    public class EnderecoResponse
+    {
+        public string? Cep { get; set; }
+        public string? Estado { get; set; }
+        public string? Cidade { get; set; }
+        public int? Regiao { get; set; }
+        public string? Rua { get; set; }
+        [JsonIgnore]
+        public string? Service { get; set; }
+    }
+}
---
+++ code/IntegraBrasil/IntegraBrasil/DTOs/ResponseGenerico.cs
@@ -0,0 +1,12 @@
+﻿using System.Dynamic;
+using System.Net;
+
+namespace IntegraBrasil.DTOs
+{
+    public class ResponseGenerico<T> where T : class
+    {
+        public HttpStatusCode CodigoHttp { get; set; }
+        public T? DadosRetorn { get; set; }
+        public ExpandoObject? ErroRetorno { get; set; }
+    }
+}
