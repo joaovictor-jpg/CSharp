d6a188112b1decc1eae1d85a39cd2b8d4caf0014
joao victor <joaovictormdasilva676@gmail.com>
2023-05-28 12:55:19 -0300

    Created new employee model

---
+++ code/FirstCors/FirstCors/Model/EmployeeModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FirstCors.Model
+{
+    [Table("employee")]
+    public class EmployeeModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Photo { get; set; }
+
+        public EmployeeModel(string name, int age, string photo)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Age = age;
+            Photo = photo;
+        }
+    }
+}
